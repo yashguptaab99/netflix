d36b8f350d2c9112b42e3e7078ab1b56223876a0
yashguptaab99 <yashguptaab99@gmail.com>
2021-03-07 02:05:43 +0530

    Home Page

---
+++ src/components/header/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link as ReactRouterLink } from "react-router-dom";
+import { Background, ButtonLink, Container, Logo } from "./styles/header";
+
+export default function Header({ bg = true, children, ...restProps }) {
+    return bg ? (
+        <Background data-testid="header-bg" {...restProps}>
+            {children}
+        </Background>
+    ) : (children);
+}
+
+Header.Frame = function HeaderFrame({ children, ...restProps}) {
+    return <Container {...restProps}>{children}</Container>;
+}
+
+Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
+  return <ButtonLink {...restProps}>{children}</ButtonLink>;
+}
+
+Header.Logo = function HeaderLog({ to, ...restProps}) {
+    return (
+        <ReactRouterLink tp={to}> 
+            <Logo {...restProps} />
+        </ReactRouterLink>
+    )
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,14 +2,35 @@ import React from "react";
 import { FaqsContainer } from "../containers/faqs";
 import { FooterContainer } from "../containers/footer";
 import { JumbotronContainer } from "../containers/jumbotron";
+import { HeaderContainer } from "../containers/header";
+import { OptForm, Feature } from "../components";
 
 export default function Home() {
 
-    return(
-        <>
-            <JumbotronContainer />
-            <FaqsContainer />
-            <FooterContainer />
-        </>
-    )
+    return (
+      <>
+        <HeaderContainer>
+            <Feature>
+            <Feature.Title>
+              Unlimited Films, TV programmes and more.
+            </Feature.Title>
+            <Feature.SubTitle>
+              Watch anywhere. Cancel at any time.
+            </Feature.SubTitle>
+            <OptForm>
+              <OptForm.Input placeholder="Email Address" />
+              <OptForm.Button>Try It now</OptForm.Button>
+              <OptForm.Break />
+              <OptForm.Text>
+                Ready to watch? Enter your email to create or restart your
+                membership
+              </OptForm.Text>
+            </OptForm>
+          </Feature>
+        </HeaderContainer>
+        <JumbotronContainer />
+        <FaqsContainer />
+        <FooterContainer />
+      </>
+    );
 }
